af5a3a4aeec65020225e62fac87cbd5e35c2755b
Farkeur <farkeur42@gmail.com>
2016-06-04 18:45:34 +0200

    debug

--- Overlord/Overlord/Program.cs
+++ Overlord/Overlord/Program.cs
@@ -8,9 +8,7 @@ namespace Overlord
     /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
