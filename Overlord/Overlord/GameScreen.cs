5bec92aef3fdc3aff2c90ef66822450ad748d548
Farkeur <farkeur42@gmail.com>
2016-06-04 16:04:20 +0200

    GameScreen

---
+++ Overlord/Overlord/GameScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Overlord
+{
+    class GameScreen
+    {
+
+        public void Initialize()
+        {
+
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
